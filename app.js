921b5e42c4c696de71f0248e9f98b20deb2c5e74
Juan Pablo <juampas@gmail.com>
2018-10-15 04:14:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(80);
+}).listen(PORT);
