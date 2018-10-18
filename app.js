78f67205da394f35ceb6ddb1cf99094bec598f00
Juan Pablo <juampas@gmail.com>
2018-10-18 16:39:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000
 var count = 0;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  console.log(`Execute : ${count++}`);
+  res.send(`App Node : ${count++} - ${request.connection.remoteAddress}`);
+  console.log(`Execute : ${count++} - ${request.connection.remoteAddress}`);
 });
 
 app.listen(PORT, function () {
