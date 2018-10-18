4b483ad9bbbbfd0fb2252399aeae926b17f4213e
Juan Pablo <juampas@gmail.com>
2018-10-18 17:03:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000
-var count = 0;
+var count = 1;
 
 app.get('/', function (req, res) {
-  res.send(`App Node : ${count++} - ${req.connection.remoteAddress}`);
+  res.send(`App Node : ${count} - ${req.connection.remoteAddress}`);
   console.log(`Execute : ${count++} - ${req.connection.remoteAddress}`);
 });
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT} : ${count++}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
