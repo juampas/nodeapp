ea52384ed8cf9a6e731bab2a84059f184e88f6c6
Juan Pablo <juampas@gmail.com>
2018-10-18 17:23:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 5000
 var count = 1;
 
 app.get('/', function (req, res) {
-  var message = `App Node Count : ${count++} <br/> My IP Address  : ${req.ip}`;
+  var ip = req.headers['x-forwarded-for'].split(',')[0];
+  var message = `App Node Count : ${count++} <br/> My IP Address  : ${ip}`;
   res.send(message);
   console.log(message);
 });
