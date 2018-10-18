733624ae6dce107a755be547eba06cccf9a58d15
Juan Pablo <juampas@gmail.com>
2018-10-18 14:02:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000
+var count = 0;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  console.log(`Execute : ${count++}`);
 });
 
 app.listen(PORT, function () {
