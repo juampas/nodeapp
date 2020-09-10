1f35a8087bc26e9f73d79df1d8697eebb877b8a0
Juan Pablo Suarez <jpablosuarez@outlook.com>
2020-09-10 12:36:22 -0300

    Update Message

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
     ipAddr = req.connection.remoteAddress;
   }
   
-  var message = `> WebAppNode Count : ${count++} <br/> My IP Address  : ${ipAddr} <`;
+  var message = `<br/> > WebAppNode Count : ${count++} < <br/> > My IP Address  : ${ipAddr} <`;
   
   res.send(message);  
   console.log(message);
